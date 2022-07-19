328a1ab22250c7b0c59108fb9e2f0dc1b5e029fb
Johan <johan@macair2020>
2022-07-19 09:34:34 +0200

    added separation

--- src/videos/videos.tsx
+++ src/videos/videos.tsx
@@ -13,6 +13,10 @@ export default () => (
 
     <hr />
 
+    <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+      Videos 2021
+    </h3>
+
     <div className={"grid grid-cols-2 gap-4"}>
       {videosAndText.map((v, i) => (
         <VideoItem key={i} v={v} i={i} />
