9b30ed8f4e0beb9717b8cc9a6afb4251e7a30e1f
Johan <johan@macair2020>
2022-07-19 09:26:30 +0200

    keep old ones

--- src/videos/videos.tsx
+++ src/videos/videos.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { videosAndText2022 } from "./data";
+import { videosAndText, videosAndText2022 } from "./data";
 
 import VideoItem from "./video-item";
 
@@ -7,6 +7,14 @@ export default () => (
   <div className="p-12 bg-white h-200">
     <div className={"grid grid-cols-2 gap-4"}>
       {videosAndText2022.map((v, i) => (
+        <VideoItem key={i} v={v} i={i} />
+      ))}
+    </div>
+
+    <hr />
+
+    <div className={"grid grid-cols-2 gap-4"}>
+      {videosAndText.map((v, i) => (
         <VideoItem key={i} v={v} i={i} />
       ))}
     </div>
